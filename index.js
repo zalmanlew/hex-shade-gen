4023a1e8acd3d9b435bb39fc3f261308018c1302
zalmanlew <35234925+zalmanlew@users.noreply.github.com>
2022-04-28 22:10:30 -0400

    formatting

--- index.js
+++ index.js
@@ -1,20 +1,36 @@
 function shadeColor(color, offset) {
-  
   // default color
-  color = color || "007aff"
-  
+  color = color || "007aff";
+
   // default offset (one shade darker)
-  offset = offset || -1
-  
+  offset = offset || -1;
+
   //normalize color
   color = color.replace("#", "").substr(0, 6).toLowerCase().split("");
 
   // ordered hex values
-  const _hex = ["0","1","2","3","4","5","6","7","8","9","a","b","c","d","e","f"];
+  const _hex = [
+    "0",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+  ];
   let new_color = "#";
 
   color.forEach(function (char) {
-    index = _hex.indexOf(char) + (offset || 0);
+    let index = _hex.indexOf(char) + (offset || 0);
 
     // if the index would cause this color to go 'out of bounds' then we leave that char as is
     if (index >= _hex.length) {
