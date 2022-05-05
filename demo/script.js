b98aabd5e003a5c75ef3848361cdf0d71fb61a7a
zalmanlew <35234925+zalmanlew@users.noreply.github.com>
2022-05-04 20:12:03 -0400

    slider to set shade level

--- demo/script.js
+++ demo/script.js
@@ -1,21 +1,32 @@
 old_obj = document.getElementById("old");
 new_obj = document.getElementById("new");
 
+old_obj.addEventListener("click", function () {
+  copyToClipboard(old_obj);
+});
+new_obj.addEventListener("click", function () {
+  copyToClipboard(new_obj);
+});
+
+function copyToClipboard(o) {
+  console.log(o.firstChild.innerText);
+  navigator.clipboard.writeText(o.firstChild.innerText);
+}
+
 // starting color - random
 color = "#000000".replace(/0/g, function () {
   return (~~(Math.random() * 16)).toString(16);
 });
 
 /* TODO
- * set shade levels to a slider
- * labels on old/new color
  * message indicating the new color is copied to clipboard
- * don't copy to clipboard on init recolor
  */
 
-function reColor(color) {
+function reColor(color, copy = true) {
+  shade = parseInt(document.getElementById("slider").value);
+
   old_color = shadeColor(color, 0); // normalize
-  new_color = shadeColor(old_color, -1); // TODO: number controlled by user
+  new_color = shadeColor(old_color, shade);
 
   Object.assign(old_obj.style, {
     backgroundColor: old_color,
@@ -31,12 +42,14 @@ function reColor(color) {
   old_obj.firstChild.innerText = old_color;
   new_obj.firstChild.innerText = new_color;
 
-  navigator.clipboard.writeText(new_color); //  TODO: don't copy on init run
-  console.log({ old_color }, { new_color });
+  if (copy) {
+    navigator.clipboard.writeText(new_color);
+  }
+  console.log({ old_color }, { shade }, { new_color });
 }
 
-// recolor on page load
-reColor(color);
+// recolor on page load - without copying to clipboard
+reColor(color, (copy = false));
 
 // listen for pasted colors and recolor boxes on paste
 document.addEventListener("paste", (event) => {
