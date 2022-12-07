19d6836f1b3edf40c25f98f690e0f39f06e06657
zalmanlew <35234925+zalmanlew@users.noreply.github.com>
2022-12-06 23:06:45 -0500

    optimized

--- index.js
+++ index.js
@@ -1,42 +1,13 @@
-function shadeColor(color = "007aff", offset = -1) {
-  // normalize color and split to array
-  let chars = color.replace("#", "").substr(0, 6).toLowerCase().split("");
-
-  // ordered hex values
-  const _hex = [
-    "0",
-    "1",
-    "2",
-    "3",
-    "4",
-    "5",
-    "6",
-    "7",
-    "8",
-    "9",
-    "a",
-    "b",
-    "c",
-    "d",
-    "e",
-    "f",
-  ];
-
-  let new_color = "#";
+function shadeColor(color = "#007aff", offset = -1) {
+  let chars = color.replace("#", "").toLowerCase().split("");
+  const hexValues = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f"];
+  let newColor = "#";
 
   chars.forEach(function (char) {
-    let index = _hex.indexOf(char) + (offset || 0);
-
-    // if the index would cause this color to go 'out of bounds' then we leave that char as is
-    if (index >= _hex.length) {
-      index = _hex.length - 1;
-    }
-    if (index < 0) {
-      index = 0;
-    }
-    
-    new_color += _hex[index];
+    let index = hexValues.indexOf(char) + (offset || 0);
+    index = Math.max(0, Math.min(hexValues.length - 1, index));
+    newColor += hexValues[index];
   });
 
-  return new_color;
+  return newColor;
 }
