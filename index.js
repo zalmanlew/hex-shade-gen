02ce6bc9a155fe483f7e785e5fdba62aeaddedb1
Zalman Lew <35234925+zalmanlew@users.noreply.github.com>
2023-01-22 18:13:58 -0500

    optimized

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
 function shadeColor(color = "#007aff", offset = -1) {
-  let chars = color.replace("#", "").toLowerCase().split("");
-  const hexValues = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f"];
   let newColor = "#";
-
-  chars.forEach(function (char) {
-    let index = hexValues.indexOf(char) + (offset || 0);
-    index = Math.max(0, Math.min(hexValues.length - 1, index));
-    newColor += hexValues[index];
-  });
-
+  for (let i = 1; i < 7; i++) {
+    let char = color[i];
+    let index = parseInt(char, 16) + offset;
+    index = Math.max(0, Math.min(15, index)).toString(16);
+    newColor += index;
+  }
   return newColor;
 }
